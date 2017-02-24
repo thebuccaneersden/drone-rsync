9407a708fe437905d32a3b05baf5210bdc93a54b
Hans-Christian Andersen <hans.andersen@phulse.com>
2017-02-23 21:55:39 -0800

    .

--- upload.sh
+++ upload.sh
@@ -101,8 +101,7 @@ if [ $? -eq 0 ]; then
     printf "Using ecdsa based key\n"
     keyfile="$home/.ssh/id_ecdsa"
 fi
-base64 --help
-echo "$SSH_KEY" | base64 --decode > $keyfile
+echo "$SSH_KEY" | base64 -d > $keyfile
 chmod 0600 $keyfile
 echo "Keyfile ="
 echo $keyfile
